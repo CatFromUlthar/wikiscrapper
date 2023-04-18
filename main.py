6c55fe4ec911ed3bb760e80d5eee1c5f60892df7
Arseniy <a9653265020@gmail.com>
2023-04-18 22:27:17 +0300

    added recompiled data to init

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ class ScrappedArticle:
     def __init__(self, url: str):
         self.url = url
         self.raw_txt = BeautifulSoup(requests.get(f'{self.url}').text, 'lxml')
+        self.recompiled_data = None
 
     def __repr__(self):
-        return f'ScrappedArticle(url={self.url}, raw_txt={self.raw_txt})'
+        return f'ScrappedArticle(url={self.url}, raw_txt={self.raw_txt}, recompiled_data={self.recompiled_data})'
 
-    def recompile(self):
+    def recompile(self) -> dict:
         url = self.url
         title = self.raw_txt.find('h1', id='firstHeading', string=True).string
         shortened_info = unicodedata.normalize('NFKD',
@@ -40,6 +41,6 @@ class ScrappedArticle:
 
 if __name__ == '__main__':
     a1 = ScrappedArticle(
-        'https://ru.wikipedia.org/wiki/%D0%9A%D1%83%D0%B4%D0%B5%D0%BB%D0%BA%D0%B0,_%D0%A0%D0%BE%D0%BB%D0%B0%D0%BD%D0%B4')
-    a1.recompile()
+        'https://ru.wikipedia.org/wiki/%D0%9F%D0%B0%D0%B3')
+    print(a1.recompile())
     a1.add_to_db()
